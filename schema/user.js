3c9674880030543b269538134c509f419ec57b42
wjq <1092229612@qq.com>
2022-09-20 16:57:42 +0800

    wjq

--- schema/user.js
+++ schema/user.js
@@ -4,11 +4,13 @@ const joi = require('joi')
 // 导入用户名和密码的验证规则
 const username = joi.string().alphanum().min(1).max(10).required()
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
+const captcha = joi.string().pattern(/^[\S]{4}$/).required()
 
 // 定义验证注册和登录表单数据的规则对象
 exports.reg_login_schema = {
     body: {
         username,
-        password
+        password,
+        captcha
     }
 }
