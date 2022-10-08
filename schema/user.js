5f02d0a86518fed39a4f7a7db148a54104e09b79
WJQ <1092229612@qq.com>
2022-10-08 11:41:43 +0800

    修改了密码校验(12位->20位)

--- schema/user.js
+++ schema/user.js
@@ -3,7 +3,7 @@ const joi = require('joi')
 
 // 导入用户名和密码的验证规则
 const username = joi.string().alphanum().min(1).max(10).required()
-const password = joi.string().pattern(/^[\S]{6,12}$/).required()
+const password = joi.string().pattern(/^[\S]{6,20}$/).required()
 const captcha = joi.string().pattern(/^[\S]{4}$/).required()
 
 // 定义验证注册和登录表单数据的规则对象
