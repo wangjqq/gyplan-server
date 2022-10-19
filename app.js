b72c4e916bb6f55c5ffb1c8a80d980b3081550fd
WJQ <1092229612@qq.com>
2022-10-19 18:58:01 +0800

    wjq

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const cors = require('cors')
 app.use(cors({
   // origin: 'https://119.91.65.198',
   // origin: 'http://127.0.0.1:8080',
-  credentials: true
+  // credentials: true
 }))
 app.use(cookieParser('secret'));
 app.use(session({
